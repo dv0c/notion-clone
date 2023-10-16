c88e69854cd122ce5d2d33a1c09159e8c3129e98
dv0c <tasosmeidanis12@gmail.com>
2023-10-17 01:23:30 +0300

    modified:   app/(main)/(routes)/documents/page.tsx 	new file:   app/(main)/_components/Item.tsx 	modified:   app/(main)/_components/Navigation.tsx 	modified:   app/layout.tsx 	modified:   convex/_generated/api.d.ts 	modified:   convex/_generated/dataModel.d.ts 	new file:   convex/documents.ts 	new file:   convex/schema.ts

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,9 +3,25 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
+
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "Note created successfully",
+      error: "Failed to create note",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -25,7 +41,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Notion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
